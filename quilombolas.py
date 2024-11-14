736f45f87557e2940c747d63ae7942f25fd80597
erikitafaria <erikitafaria@yahoo.com.br>
2024-11-14 10:51:12 -0300

    quilombolas.py

--- quilombolas.py
+++ quilombolas.py
@@ -3,13 +3,20 @@ import pandas as pd
 
 st.title('Localização das comunidades quilombolas (2022)')
 df = pd.read_csv('https://raw.githubusercontent.com/adrianalite/datasets/main/BR_LQs_CD2022.csv')
+
 df.drop(columns=['Unnamed: 0'], inplace=True)
+
 list = ['Lat_d', 'Long_d']
 df[list] = df[list].apply(pd.to_numeric, errors='coerce')
+
 estados = df['NM_UF'].unique()
 estadoselecionado = st.selectbox('Qual estado selecionar?',estados)
+
 dadosfiltrados = df[df['NM_UF'] == estadoselecionado]
+
 if st.checkbox('Mostrar tabela')==True:
   st.write(dadosfiltrados)
 
 st.map(dadosfiltrados, latitude="Lat_d", longitude="Long_d")
+st.bar_chart(df['NM_UF'].value_counts()) #gráfico de barras por estado
+st.bar_chart(df['NM_MUNIC'].value_counts()[:10])
